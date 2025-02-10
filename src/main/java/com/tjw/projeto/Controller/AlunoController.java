bb5786f22d6b68b59c4f401d161ce56be7e8bbfa
Felipe Amorim <felipe21fab@gmail.com>
2025-02-10 15:55:12 -0300

    Loads cadastrar aluno page.

---
+++ src/main/java/com/tjw/projeto/Controller/AlunoController.java
@@ -0,0 +1,34 @@
+package com.tjw.projeto.Controller;
+
+import com.tjw.projeto.entity.Aluno;
+import com.tjw.projeto.serviceImpl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl alunoService;
+
+    @GetMapping("/teste")
+    String teste(ModelMap model){
+        return "teste";
+    }
+
+    @GetMapping("/listar")
+    String listarAlunos(ModelMap model){
+        model.addAttribute("alunos", alunoService.buscarTodos());
+        return "aluno/lista";
+    }
+
+    @GetMapping("/cadastrar")
+    String cadastrarAluno(ModelMap model){
+        model.addAttribute("aluno", new Aluno());
+        return "aluno/cadastro";
+    }
+}
---
+++ src/main/java/com/tjw/projeto/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tjw.projeto.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    String index(){
+        return "home";
+    }
+}
